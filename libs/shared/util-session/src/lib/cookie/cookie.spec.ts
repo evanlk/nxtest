b2e027f6ea3694ea1b7a94c4dee795f6fe36c77e
Evan Kennedy <kennedye@ctinetworks.com>
2021-03-26 14:16:41 -0400

    initial commit

---
+++ libs/shared/util-session/src/lib/cookie/cookie.spec.ts
@@ -0,0 +1,7 @@
+// import { cookie } from './cookie';
+
+// describe('cookie', () => {
+//   it('should work', () => {
+//     expect(cookie()).toBe('cookie');
+//   });
+// });
---
+++ libs/shared/util-session/src/lib/session/session.spec.ts
@@ -0,0 +1,7 @@
+// import 'session' from './session';
+
+// describe('session', () => {
+//   it('has to work', () => {
+//     expect(session()).toBe('session');
+//   });
+// })
