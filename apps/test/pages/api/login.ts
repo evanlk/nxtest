575f213af9d8dca3acbc66b4d37298ed79cc1e16
Evan Kennedy <kennedye@ctinetworks.com>
2021-03-26 15:35:24 -0400

    Revert "narrowed down to the issue.  It appears async/await is causing the 'regeneratorRuntime' error.  However, async/await has worked on previous migrations of nx"
    This reverts commit 96c55a0062b2d4ec52065a9004bcd7d2ef9560df.

--- apps/test/pages/api/login.ts
+++ apps/test/pages/api/login.ts
@@ -2,8 +2,13 @@ import connect from 'next-connect';
 
 import { createSessionMiddleware } from '@nxtest/shared/util-session';
 
+const authenticate = async () => {
+  return { user: { id: 1, name: 'John Smith' } };
+};
+
 export default connect()
-  .use(createSessionMiddleware())
-  .get((req: any, res: any) => {
+  .use(createSessionMiddleware(authenticate))
+  .get((req, res) => {
     res.json({ status: 'success' });
+    s;
   });
